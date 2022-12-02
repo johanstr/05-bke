5205bb70c6836963120626fa188f5dfe6c6fba62
Johan Strootman <johanstr@users.noreply.github.com>
2022-12-02 14:45:19 +0100

    buttonClick finetunen

--- js/bke.js
+++ js/bke.js
@@ -81,30 +81,21 @@ function buttonClick(event_element)
         de tekst 'Reset ronde' op de button staat.
      */
     if(event_element.target.innerHTML == 'Start ronde') {
-        /*  Ja, dus gaan we een ronde starten en ondernemen de nodige
-         *  stappen, zoals:
-         *      - Ronde nummer verhogen
-         *      - Ronde nummer tonen op het scherm
-         *      - click events toevoegen aan de cellen
-         *      - Tekst veranderen op de button
-         *      - Ronde timer starten
-         */
 
         current_round++;                                        // Ronde nummer met 1 verhogen           
         element_rounds_played.innerHTML = current_round;        // Nu plaatsen we het nummer in het element
                                                                 // om het te tonen aan de spelers
         
+        // Willekeurig bepalen welke speler mag beginnen, dit levert een 1(speler 1) of een 2(speler 2) op
+        current_player = Math.floor(Math.random() * (2 - 1 + 1) + 1);  
+        
+        // Nu gaan we tonen op het scherm welke speler de ronde mag beginnen
+        element_turn_number.innerHTML = current_player;     // Het nummer tonen
+        element_turn_image.src = (current_player == 1 ? 'img/circle200x200.png' : 'img/cross200x200.png');
+        
         /*
             In de onderstaande programma lus lopen we nu langs alle cellen in het speelbord
             en koppelen hier een click eventhandler aan.
-
-            Een oude (niet aan te bevelen) manier zie je hieronder:
-
-            for(let index = 0; index < element_cells.length; index++) {
-                element_cells[index].addEventListener('click', cellClick);
-            }
-
-            De nieuwe (aan te bevelen) manier staat hieronder:
         */
         element_cells.forEach(cell => {
             cell.addEventListener('click', cellClick);
@@ -137,12 +128,6 @@ function buttonClick(event_element)
         
         /*  
          *  We gaan nu eerst alle cellen in het speelveld onklikbaar maken
-         *
-         *  Oude manier:
-         *  for(var kolom = 0; kolom < element_cells.length; kolom++) {
-         *     element_cells[kolom].src = 'img/empty.jpg';
-         *     element_cells[kolom].removeEventListener('click', cellClick);
-         *  }
          */
         element_cells.forEach(cell => {
             cell.removeEventListener('click', cellClick);
@@ -165,7 +150,7 @@ function buttonClick(event_element)
  */
 function cellClick(event_element)
 {
-    alert('Op een cel geklikt');
+    
 }
 
 /*
