d526cb70c61f1df864fcb78e589115a4a84a5849
Johan Strootman <johanstr@users.noreply.github.com>
2022-12-02 10:26:02 +0100

    ButtonClick functie programmeren

--- js/bke.js
+++ js/bke.js
@@ -75,5 +75,105 @@ window.onload = function() {
  */
 function buttonClick(event_element)
 {
+    /* 
+        We controleren hier of de tekst op de button gelijk is aan: 'Start ronde'
+        Want als dat zo is dan moet er namelijk iets anders gebeuren dan wanneer
+        de tekst 'Reset ronde' op de button staat.
+     */
+    if(event_element.target.innerHTML == 'Start ronde') {
+        /*  Ja, dus gaan we een ronde starten en ondernemen de nodige
+         *  stappen, zoals:
+         *      - Ronde nummer verhogen
+         *      - Ronde nummer tonen op het scherm
+         *      - click events toevoegen aan de cellen
+         *      - Tekst veranderen op de button
+         *      - Ronde timer starten
+         */
 
+        current_round++;                                        // Ronde nummer met 1 verhogen           
+        element_rounds_played.innerHTML = current_round;        // Nu plaatsen we het nummer in het element
+                                                                // om het te tonen aan de spelers
+        
+        /*
+            In de onderstaande programma lus lopen we nu langs alle cellen in het speelbord
+            en koppelen hier een click eventhandler aan.
+
+            Een oude (niet aan te bevelen) manier zie je hieronder:
+
+            for(let index = 0; index < element_cells.length; index++) {
+                element_cells[index].addEventListener('click', cellClick);
+            }
+
+            De nieuwe (aan te bevelen) manier staat hieronder:
+        */
+        element_cells.forEach(cell => {
+            cell.addEventListener('click', cellClick);
+        });
+
+        event_element.target.innerHTML = 'Reset ronde';     // Tekst op de knop veranderen
+        element_round_time.innerHTML = '00:00';             // We resetten en tonen een ronde tijd 00:00
+        
+        /*
+         * We starten nu de ronde timer
+         * Als echter de variabele timer_id niet gelijk is aan NULL dan loopt er nog 
+         * een timer, deze moeten we eerst stoppen.
+         */
+        if(timer_id != null) {          // Als de variabele timer_id niet NULL is is er al een timer
+            clearInterval(timer_id);    // Stop de timer
+            timer_id = null;            // Reset de variabele op NULL
+            elapsedTimeInSeconds = 0;   // Reset de variabele op 0 seconden
+        }
+        
+        timer_id = setInterval( roundsTimer, 1000 );    // Om de seconde mag de functie roundsTimer uitgevoerd worden
+    } else {
+        /*  Nee, dan gaan we het spel resetten door de volgende
+         *  stappen uit te voeren:
+         *      - Timer stoppen
+         *      - Laatste timer info tonen op het scherm
+         *      - Speelveld leeg maken (overal weer empty.jpg in plaatsen) en
+         *        Deactiveren van de click event handlers op de cellen
+         *      - Start spel op de button plaatsen
+         */
+        
+        /*  
+         *  We gaan nu eerst alle cellen in het speelveld onklikbaar maken
+         *
+         *  Oude manier:
+         *  for(var kolom = 0; kolom < element_cells.length; kolom++) {
+         *     element_cells[kolom].src = 'img/empty.jpg';
+         *     element_cells[kolom].removeEventListener('click', cellClick);
+         *  }
+         */
+        element_cells.forEach(cell => {
+            cell.removeEventListener('click', cellClick);
+            cell.src = 'img/empty.jpg';
+        });
+        
+        event_element.target.innerHTML = 'Start ronde';     // Tekst op de knop veranderen
+
+        // Nu stoppen we de ronde timer
+        if(timer_id != null) {          // Als de variabele timer_id niet NULL is is er al een timer
+            clearInterval(timer_id);    // Stop de timer
+            timer_id = null;            // Reset de variabele op NULL
+            elapsedTimeInSeconds = 0;   // Reset de variabele op 0 seconden
+        }
+    }
+}
+
+/*
+ * Deze functie handelt iedere klik op een speelveld cel af.
+ */
+function cellClick(event_element)
+{
+    alert('Op een cel geklikt');
+}
+
+/*
+ * In deze functie laten we steeds de ronde tijd op het scherm zien
+ * Dit doen we in een eigen functie, omdat we de tijd nog moeten
+ * vormgeven in de vorm 00:00. Dit is namelijk niet standaard.
+ */
+function roundsTimer()
+{
+    
 }
